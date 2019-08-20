c98dcca48d88b2412ca82bdc97cf2636b4b467ab
PC-01 <PC-01@PC-01-PC>
2019-08-20 19:28:05 +0900

    4-12남음

--- src/z_exam/Exam_04.java
+++ src/z_exam/Exam_04.java
@@ -2,23 +2,33 @@ package z_exam;
 
 public class Exam_04 {
 	public static void main(String[] args) {
-	
+		
 		/*
 		  
 		  [4-1] 다음의 문장들을 조건식으로 표현하라.
-			1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식
-			2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식
-			3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식
-			4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식
-			5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식
-			6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지
+			1. int형 변수 x가 10보다 크고 20보다 작을 때 true인 조건식			10 < x && x < 20
+			2. char형 변수 ch가 공백이나 탭이 아닐 때 true인 조건식			!( ch==' ' && ch == '\t')
+			3. char형 변수 ch가 ‘x' 또는 ’X'일 때 true인 조건식			ch == 'x' && ch == 'X'
+			4. char형 변수 ch가 숫자(‘0’~‘9’)일 때 true인 조건식			'0' <= ch && ch <= '9'
+			5. char형 변수 ch가 영문자(대문자 또는 소문자)일 때 true인 조건식	'A' <= ch && ch<='Z' || 'a' <= ch && ch <= 'z'
+			6. int형 변수 year가 400으로 나눠떨어지거나 또는 4로 나눠떨어지고 100으로 나눠떨어지지  year % 400 == 0 || (year % 4 && year % 100 !=0)
 			않을 때 true인 조건식
-			7. boolean형 변수 powerOn가 false일 때 true인 조건식
-			8. 문자열 참조변수 str이 “yes”일 때 true인 조건식
+			7. boolean형 변수 powerOn가 false일 때 true인 조건식			!(powerOn)
+			8. 문자열 참조변수 str이 “yes”일 때 true인 조건식				str=="yes"
 		  
 		  [4-2] 1부터 20까지의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
-
+		  	답 : 
+		  	int sum = 0;
+		  	
+		  	for(int i = 1; i < 21; i++){
+		  		if(!(i % 2 == 0 || i % 3 == 0)){
+		  			sum += i;
+		  		}
+		  	}
+			System.out.println(sum);
+			
 		  [4-3] 1+(1+2)+(1+2+3)+(1+2+3+4)+...+(1+2+3+...+10)의 결과를 계산하시오.
+		  	답 : 
 		  	int result = 0;
 		
 			for(int i = 1 ; i<=10; i++){
@@ -41,13 +51,15 @@ public class Exam_04 {
 		  	System.out.println("결과값 : "+result);
 		  	
 		  [4-4] 1+(-2)+3+(-4)+... 과 같은 식으로 계속 더해나갔을 때, 몇까지 더해야 총합이 100이상이 되는지 구하시오.
+		   답 : 
 			int sw = 1;
 			int sum = 0;
-			int i;
+			int i = 1;
 		
-			for(i = 1; sum<=100; i++){
+			while(sum < 100){
 				sum += i*sw;
 				sw = -sw;
+				i++;
 			}
 		
 			System.out.println(i);
@@ -80,12 +92,13 @@ public class Exam_04 {
 			}
 				
 		[4-6] 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하는 프로그램을 작성하시오.
+		답 :
 			int dice1;
 			int dice2;
 			
 			for(dice1 = 1;dice1 <= 6;dice1++){
 				for(dice2 = 1; dice2 <= 6; dice2++){
-					if(dice1+dice2 == 10)
+					if(dice1+dice2 == 6)
 						System.out.println(dice1+","+dice2);
 				}
 			}
@@ -116,14 +129,23 @@ public class Exam_04 {
 			  
 			  class Exercise4_9 {
 				public static void main(String[] args) {
+				
 					String str = "12345";
-						int sum = 0;
-						for(int i=0; i < str.length(); i++) {
-							sum += charAt(i);
-						}
-					System.out.println("sum="+sum);
+					int sum = 0;
+					
+					for(int i=0; i < str.length(); i++) {	
+				
+					(1) 알맞은 코드를 넣어 완성하시오.
+					
 					}
-			  	}	
+					
+					System.out.println("sum="+sum);
+				}
+			  }	
+		답 : 
+			for(int i=0; i < str.length(); i++) {
+					sum += str.charAt(i)-48;
+			}
 			  	
 		[4-10] int타입의 변수 num 이 있을 때, 각 자리의 합을 더한 결과를 출력하는 코드를완성하라. 
 		만일 변수 num의 값이 12345라면, ‘1+2+3+4+5’의 결과인 15를 출력하라. 
@@ -202,19 +224,66 @@ public class Exam_04 {
 		8*2=16 9*2=18
 		8*3=24 9*3=27
 		
-		for(int j = 1; j < 4; j++){
-			for(int i = 2; i < 9 < i+=3){
-				System.out.println("");
+		[4-13] 다음은 주어진 문자열(value)이 숫자인지를 판별하는 프로그램이다. (1)에 알맞
+		은 코드를 넣어서 프로그램을 완성하시오.
+		class Exercise4_13{
+			public static void main(String[] args){
+				String value = "12o34";
+				char ch = ' ';
+				boolean isNumber = true;
+				
+				// 반복문과 charAt(int i)를 이용해서 문자열의 문자를
+				// 하나씩 읽어서 검사한다.
+				for(int i=0; i < value.length() ;i++) {
+				
+				(1) 알맞은 코드를 넣어 완성하시오.
+				
+				}
+				if (isNumber) {
+					System.out.println(value+"는 숫자입니다.");
+				} else {
+					System.out.println(value+"는 숫자가 아닙니다.");
+				}
+			} // end of main
+		} // end of class
+		
+		답 : 
+			ch =  value.charAt(i);
+			if(!('0'<= ch && ch <='9')){
+				isNumber = false;
 			}
+		
+		
+		
+		
+		[4-15] 다음은 회문수를 구하는 프로그램이다. 회문수(palindrome)란, 숫자를 거꾸로 읽
+		어도 앞으로 읽는 것과 같은 수를 말한다. 예를 들면 ‘12321’이나 ‘13531’같은 수를 말한
+		다. (1)에 알맞은 코드를 넣어서 프로그램을 완성하시오.
+		[Hint] 나머지 연산자를 이용하시오.
+		
+		class Exercise4_15{
+		public static void main(String[] args){
+				int number = 12321;
+				int tmp = number;
+				int result =0; // 변수 number를 거꾸로 변환해서 담을 변수
+				
+				while(tmp !=0) {
+					result = result * 10 + tmp % 10;
+					tmp /= 10;
+				}
+				
+				if(number == result)
+				System.out.println( number + "는 회문수 입니다.");
+				
+				else
+				System.out.println( number + "는 회문수가 아닙니다.");
+			} // main
 		}
 		
-					
+		답 : 
+		result = result * 10 + tmp % 10;
+		tmp /= 10;
+		
 		*/
-		for(int j = 1; j < 4; j++){
-			for(int i = 2; i < 9; i++){
-				System.out.println(i+"*"+j+"="+i*j);
-			}
-		}
-		
 	}
 }
